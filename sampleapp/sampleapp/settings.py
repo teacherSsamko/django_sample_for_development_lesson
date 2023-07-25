39b03f045d7c4cac48da6e683df54750506d07dd
teacherSsamko <ssamko@kakao.com>
2023-07-25 14:10:12 +0900

    remote postgresql setup

--- sampleapp/sampleapp/settings.py
+++ sampleapp/sampleapp/settings.py
@@ -82,12 +82,18 @@ WSGI_APPLICATION = 'sampleapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+        'OPTIONS': {
+            'options': '-c search_path=sample,public'
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
