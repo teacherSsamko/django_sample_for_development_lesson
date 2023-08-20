05affd3ee0b7625d964b3e773db220bc5b9a347b
Eunsub Lee <ssamko@kakao.com>
2023-08-20 19:51:42 +0900

    FEAT blog

---
+++ sampleapp/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.title)
