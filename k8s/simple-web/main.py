6f96a2da6bc7833c4373ca354b40d0b48764068b
Eunsub Lee <ssamko@kakao.com>
2023-08-30 14:43:19 +0900

    k8s.simple-web: liveness

--- k8s/simple-web/main.py
+++ k8s/simple-web/main.py
@@ -3,16 +3,26 @@ import socket
 
 
 class SimpleHandler(BaseHTTPRequestHandler):
+    request = 0
+
     def do_GET(self):
-        if self.path == "/":
+        if self.path == "/" and SimpleHandler.request < 5:
             self.send_response(200)
+            SimpleHandler.request += 1
             self.send_header("Content-type", "text/html")
             self.end_headers()
 
             host_name = socket.gethostname()
-            message = f"Host Name: {host_name} / v=1"
+            message = (
+                f"Host Name: {host_name} / v=1 / Request: {SimpleHandler.request} /\n"
+            )
 
             self.wfile.write(message.encode())
+        elif SimpleHandler.request >= 5:
+            self.send_response(500)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("Too many requests".encode())
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/html")
@@ -29,4 +39,5 @@ def run(server_class, handler_class, port):
 
 if __name__ == "__main__":
     port_number = 8000
+    print("Simple Web Server starting...")
     run(HTTPServer, SimpleHandler, port_number)
